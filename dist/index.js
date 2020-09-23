0e5b67947c9ebcbd78f23d80f99c397f89524445
velveet <velveet@pm.me>
2020-09-22 21:53:34 -0500

    [routes file added, imported on the index.ts main file]

--- dist/index.js
+++ dist/index.js
@@ -6,6 +6,8 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const app = express_1.default();
 const port = process.env.PORT || 3000;
+const index_1 = __importDefault(require("./routes/index"));
+app.use(index_1.default);
 app.listen(port, () => {
     console.log('Server on port !', port);
 });
--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,10 @@ import express from 'express'
 const app = express();
 const port = process.env.PORT || 3000;
 
+import indexRoutes from './routes/index'
+
+app.use(indexRoutes);
+
 app.listen(port, ()=>{
     console.log('Server on port !', port);
     
