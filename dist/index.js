2fd9bc378b5291517be00fd27401a8e497795c01
velveet <velveet@pm.me>
2020-09-22 20:53:17 -0500

    [express port config].

--- dist/index.js
+++ dist/index.js
@@ -5,6 +5,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const app = express_1.default();
-app.listen(3000, () => {
-    console.log('Hello world !');
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log('Server on port !', port);
 });
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
 import express from 'express'
 const app = express();
+const port = process.env.PORT || 3000;
 
-app.listen(3000, ()=>{
-    console.log('Hello world !');
+app.listen(port, ()=>{
+    console.log('Server on port !', port);
     
 })
