d6fc6e993c75794b8c48cd0258b1c16d166a9242
velveet <velveet@pm.me>
2020-09-22 21:42:58 -0500

    [database.sql added & commands registered], [postgresql installed on mac through homebrew], [postgresql database created], [testing data created]

---
+++ database/database.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE typescriptdatabase;
+
+CREATE TABLE users(
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(40),
+    email TEXT
+);
+
+INSERT INTO users (name, email)
+    VALUES  ('joe', 'joe@ibm.com'),
+            ('ryan', 'ryan@ibm.com');
