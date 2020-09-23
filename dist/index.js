caf696a99f48c8e64a9b204613a6a4afc0cb7a7a
velveet <velveet@pm.me>
2020-09-22 21:58:13 -0500

    [json middleware for processing json formats from APIs], [urlencoded middleware for processing from HTML forms]

--- dist/index.js
+++ dist/index.js
@@ -7,6 +7,9 @@ const express_1 = __importDefault(require("express"));
 const app = express_1.default();
 const port = process.env.PORT || 3000;
 const index_1 = __importDefault(require("./routes/index"));
+//middlewares
+app.use(express_1.default.json());
+app.use(express_1.default.urlencoded({ extended: false }));
 app.use(index_1.default);
 app.listen(port, () => {
     console.log('Server on port !', port);
--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,10 @@ const port = process.env.PORT || 3000;
 
 import indexRoutes from './routes/index'
 
+//middlewares
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
 app.use(indexRoutes);
 
 app.listen(port, ()=>{
